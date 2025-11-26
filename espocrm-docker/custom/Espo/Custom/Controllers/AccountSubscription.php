c63a79288d75150bcff3d07b56805fd92d8c1b3c
Kieran Hiett <kieranhiett@Kierans-MacBook-Pro.local>
2025-11-26 16:44:40 +0000

    Temporarily disable auth for testing

--- espocrm-docker/custom/Espo/Custom/Controllers/AccountSubscription.php
+++ espocrm-docker/custom/Espo/Custom/Controllers/AccountSubscription.php
@@ -4,11 +4,14 @@ namespace Espo\Custom\Controllers;
 
 class AccountSubscription extends \Espo\Core\Controllers\Base
 {
+    protected $authRequired = false; // TEMPORARY - for testing only
+    
     public function getActionRead($params, $data, $request)
     {
         return [
             'message' => 'hello from AccountSubscription controller',
-            'timestamp' => date('Y-m-d H:i:s')
+            'timestamp' => date('Y-m-d H:i:s'),
+            'params' => $params
         ];
     }
 }
